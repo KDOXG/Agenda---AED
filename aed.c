3a2c622bf74d50e9ce6dfb646f7532ec1de3f96d
KDOXG <26307688+KDOXG@users.noreply.github.com>
2018-08-27 14:52:32 -0300

    Add files via upload

---
+++ aed.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+void *pBuffer=malloc(1);
+
+void reOrganizar();
+{
+//tô vendo que vou tomar muito na jabiraca com isso aqui p_p
+}
+
+void incluirPessoa()
+{
+	*b=malloc()
+}
+
+void apagarPessoa()
+{
+
+}
+
+void buscarPessoa()
+{
+
+}
+
+void listarPessoa()
+{
+
+}
+
+void Sair()
+{
+
+}
+
+void main()
+{
+/*
+	int *p, *q, *i, *j, *k, *l, *v, *w;
+	char *a, **b, *c, *d, *e;
+	*/
+	scanf("%d", p);
+	switch (*p)
+	{
+		case 1:
+			incluirPessoa();
+		break;
+		case 2:
+			apagarPessoa();
+		break;
+		case 3:
+			buscarPessoa();
+		break;
+		case 4:
+			listarPessoa();
+		break;
+		case 5:
+			Sair();
+		break;
+	return;
+	}
+}
